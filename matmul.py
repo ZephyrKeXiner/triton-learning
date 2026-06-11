80414d34d157ee0d09a345b7469220985a0004da
vast.ai <aruiskyhappy2@outlook.com>
2026-06-11 13:42:26 +0000

    compelete matmul kernel

--- matmul.py
+++ matmul.py
@@ -7,6 +7,7 @@ import triton.language as tl
 def mul_kernel(
   x_ptr,
   y_ptr,
+  output_ptr,
   n_elements,
   BLOCK_M: tl.constexpr,
   BLOCK_N: tl.constexpr,
@@ -18,7 +19,14 @@ def mul_kernel(
   offs_n = pid_n * BLOCK_N + tl.arange(0, BLOCK_N)
   offs_k = tl.arange(0, BLOCK_K)
 
-  acc = tl.zeros
+  acc = tl.zeros((BLOCK_M, BLOCK_N), tl.float32)
+
+  for k in range(0, K, BLOCK_K):
+    a = tl.load(A + offs_m[:, None] * stride_am + offs_k[None, :] * stride_ak)
+    b = tl.load(B + offs_k[:, None] * stride_bk + offs_n[None, :] * stride_bn)
+    acc += tl.dot(a, b)
+  
+  tl.store(output_ptr+offs_k, acc)
 
 
 def mul(x: torch.Tensor, y: torch. Tensor):
