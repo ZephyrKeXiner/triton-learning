479f43bee3657ada319282a137bc2a38c9112214
vast.ai <aruiskyhappy2@outlook.com>
2026-06-11 13:38:27 +0000

    add cal kernel

--- matmul.py
+++ matmul.py
@@ -8,8 +8,18 @@ def mul_kernel(
   x_ptr,
   y_ptr,
   n_elements,
-  BLOCK_SIZE: tl.constexpr
-)
+  BLOCK_M: tl.constexpr,
+  BLOCK_N: tl.constexpr,
+  BLOCK_K: tl.constexpr
+):
+  pid_m = tl.program_id(0)
+  pid_n = tl.program_id(0)
+  offs_m = pid_m * BLOCK_M + tl.arange(0, BLOCK_M)
+  offs_n = pid_n * BLOCK_N + tl.arange(0, BLOCK_N)
+  offs_k = tl.arange(0, BLOCK_K)
+
+  acc = tl.zeros
+
 
 def mul(x: torch.Tensor, y: torch. Tensor):
-  
+  pass
